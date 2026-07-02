526d9ddcde7e3185cc8b16cce9246a3bcee57cf7
Premananthan Thanoyan <premananthanthanoyan@gmail.com>
2026-07-02 11:38:39 +0530

    code added in auth-controller.py file

---
+++ Backend/app/controllers/auth_controller.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, HTTPException
+from ..schemas import UserCreate, UserLogin
+from ..database import users_collection
+from ..security import hash_password, verify_password
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+@router.post("/register")
+async def register(user: UserCreate):
+
+    existing_user = await users_collection.find_one({"email": user.email})
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already exists"
+        )
+
+    new_user = {
+        "full_name": user.full_name,
+        "email": user.email,
+        "password": hash_password(user.password)
+    }
+
+    result = await users_collection.insert_one(new_user)
+
+    return {
+        "message": "User registered successfully",
+        "user_id": str(result.inserted_id)
+    }
+
+
