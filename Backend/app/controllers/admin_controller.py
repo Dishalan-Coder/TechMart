f28c32a0eb4d1362632451b6d709cb220d38266c
Premananthan Thanoyan <premananthanthanoyan@gmail.com>
2026-07-02 13:39:29 +0530

    changes added

--- Backend/app/controllers/admin_controller.py
+++
@@ -1,9 +0,0 @@
-from app.services import admin_service
-
-
-async def get_dashboard_stats() -> dict:
-    return await admin_service.get_dashboard_stats()
-
-
-async def list_users() -> list[dict]:
-    return await admin_service.list_users()
